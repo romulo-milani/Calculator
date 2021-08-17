ceeb0c90d96eb7403686fe75b7fe3aebff16e77b
Rômulo Milani <romulodmilani@gmail.com>
2021-08-17 10:51:31 -0300

    added basic math functions and operate function

---
+++ script.js
@@ -0,0 +1,37 @@
+//basic math functions
+function add(a, b) {
+    return a+b;
+}
+
+function subtract(a, b) {
+    return a-b;
+}
+
+function multiply(a, b) {
+    return a*b;
+}
+
+function divide(a, b) {
+    return a/b;
+}
+
+//main operate functon
+function operate(operator, a, b) {
+    switch(operator) {
+        case "+": {
+            return add(a, b);
+        } break;
+        case "-": {
+            return subtract(a, b);
+        } break;
+        case "*": {
+            return multiply(a, b);
+        } break;
+        case "/": {
+            return divide(a, b);
+        } break;
+        default: {
+            return "Operação inválida"
+        }
+    }
+}
